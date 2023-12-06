ff8537edd1b1b5a2b460e48b6608dca750dd6046
Yusuf Yacoobali <yyacoobali@gmail.com>
2023-12-06 17:22:54 +0000

    evolve the population

--- gym_cooking/utils/map.py
+++ gym_cooking/utils/map.py
@@ -6,9 +6,9 @@ class Map:
         self.arglist = arglist
         self.num_objects = num_objects
         self.file_path = file_path
-        self.object_chars = "tlp-/*"
+        self.object_chars = "tlp----/* "
         self.layout = None
-        self.population_size = 3
+        self.population_size = 10
         self.num_generations = 3
         self.population = None
         random.seed()
@@ -38,23 +38,6 @@ class Map:
     def calculate_collab_fitness(self, map):
         blocked_score = 0
         rows, cols = self.height, self.width
-
-        def flood_fill(row, col, visited):
-            if row < 0 or row >= rows or col < 0 or col >= cols or visited[row][col] or map[row][col] != ' ':
-                return 0
-        
-            visited[row][col] = True
-        
-            count = 1
-            count += flood_fill(row + 1, col, visited)
-            count += flood_fill(row - 1, col, visited)
-            count += flood_fill(row, col + 1, visited)
-            count += flood_fill(row, col - 1, visited)
-            # count += flood_fill(row + 1, col + 1, visited)  # Diagonal movement
-            # count += flood_fill(row - 1, col - 1, visited)  # Diagonal movement
-        
-            return count
-
         visited = [[False for _ in range(cols)] for _ in range(rows)]
 
         def find_object_positions(layout):
@@ -109,48 +92,148 @@ class Map:
             for col in range(cols):
                 if not visited[row][col] and map[row][col] == ' ':
                     # If an unvisited empty space is found, perform flood-fill from that position
-                    count = flood_fill(row, col, visited)
+                    count = self.flood_fill(row, col, visited, map)
                     if count > 0:
                         separated_regions += 1
-        self.print_map(map)
-    
-        if separated_regions > 1:
-            separated_regions = 10
+        #self.print_map(map)
+        
+        #print("separated regions:", separated_regions)
+        if separated_regions == 2:
+            separated_regions = distance_score*0.5
         collaboration_score = separated_regions + distance_score
-        print("Collaboration Fitness:", collaboration_score)
+        #print("Collaboration Fitness:", collaboration_score)
         # print("separated regions:", separated_regions)
         # print("dist Score:", distance_score)
-        return collaboration_score # Su
+        return collaboration_score, separated_regions # Su
+    
+    def flood_fill(self, row, col, visited, layout):
+        """
+        Perform a flood-fill from the given position and mark all directly connected empty spaces.
+        """
+        rows, cols = self.height, self.width
+        if row < 0 or row >= rows or col < 0 or col >= cols or visited[row][col] or layout[row][col] != ' ':
+            return 0  # Return 0 if the current position is out of bounds or not an empty space
 
+        visited[row][col] = True
 
-    def crossover(self, other_map):
-        # Implement crossover operation between two maps
-        # Exchange segments of genetic information (map layout)
-        pass
+        # Recursively perform flood-fill in all directions
+        count = 1  # Initialize count to 1 for the current empty space
+        count += self.flood_fill(row + 1, col, visited, layout)
+        count += self.flood_fill(row - 1, col, visited, layout)
+        count += self.flood_fill(row, col + 1, visited, layout)
+        count += self.flood_fill(row, col - 1, visited, layout)
 
-    def mutate(self):
-        # Implement mutation operation on the map
-        # Introduce random changes to encourage diversity
-        pass
+        return count
+
+    def crossover(self, map1, map2):
+        # Perform crossover between two parents to create a child
+        child_layout = [self.crossover_row(row1, row2) for row1, row2 in zip(map1, map2)]
+        #print(random.randint(1,10))
+        return child_layout
+
+    def crossover_row(self, row1, row2):
+        # Select a random crossover point along the row length
+        crossover_point = random.randint(1, min(len(row1), len(row2)) - 1)
+        # Combine the left part of row1 and the right part of row2
+        child_row = row1[:crossover_point] + row2[crossover_point:]
+        return child_row
+
+    def mutate(self, layout):
+        rows, cols = len(layout), len(layout[0])
+        mutation_rate = 0.001  # Adjust this as needed
+
+        while True:
+            #print("in loop")
+            # Apply mutation to the layout
+            for i in range(rows):
+                for j in range(cols):
+                    if random.random() < mutation_rate:
+                        # Mutate the character at this position
+                        new_character = random.choice(self.object_chars)
+                        layout[i][j] = new_character
+
+            layout = self.avoid_crowding(layout)
+            # Check the number of separated regions
+            visited = [[False for _ in range(cols)] for _ in range(rows)]
+            separated_regions = 0
+
+            for row in range(rows):
+                for col in range(cols):
+                    if not visited[row][col] and layout[row][col] == ' ':
+                        # If an unvisited empty space is found, perform flood-fill from that position
+                        count = self.flood_fill(row, col, visited, layout)
+                        if count > 0:
+                            separated_regions += 1
+
+            if separated_regions == 2 or separated_regions == 3:
+                break  # Exit the loop if the layout has exactly 2 separated regions
+
+        return layout
+    
+    def avoid_crowding(self, layout, density_threshold=0.5, max_iterations=100):
+        rows, cols = len(layout), len(layout[0])
+        empty_count = sum(row.count(' ') for row in layout)
+        total_cells = rows * cols
+        empty_density = empty_count / total_cells
+        iterations = 0
+
+        while empty_density < density_threshold and iterations < max_iterations:
+            #print("trying to reduce overcrowding")
+            # Find a non-empty cell and swap it with an empty cell
+            non_empty_cells = [(i, j) for i in range(rows) for j in range(cols) if layout[i][j] != ' ']
+            if non_empty_cells:
+                i, j = random.choice(non_empty_cells)
+                empty_cell = [(x, y) for x in range(rows) for y in range(cols) if layout[x][y] == ' ']
+                if empty_cell:
+                    x, y = random.choice(empty_cell)
+                    layout[i][j], layout[x][y] = layout[x][y], layout[i][j]
+
+            # Recalculate empty density
+            empty_count = sum(row.count(' ') for row in layout)
+            empty_density = empty_count / total_cells
+
+            iterations += 1
+
+        return layout
 
     def evolve_population(self, selected_maps):
         next_generation = []
-
+        crossover_children = []
+        mutated_children = []
+        #make next gen same size as prev pop
         while len(next_generation) < len(self.population):
             # Choose parents for crossover
             parent1 = random.choice(selected_maps)
             parent2 = random.choice(selected_maps)
-
             # Perform crossover to create offspring
-            child = parent1.crossover(parent2)
-
+            child = self.crossover(parent1[0], parent2[0])
+            crossover_children.append(child)
             # Apply mutation to the child
-            child.mutate()
-
+            mutated_child = self.mutate(child)
+            mutated_children.append(mutated_child)
             # Add the child to the next generation
-            next_generation.append(child)
+            next_generation.append(mutated_child)
 
+        print("CROSSOVER CHILDREN:")
+        self.print_horizontal_maps(crossover_children)
+
+        print("\nMUTATED CHILDREN:")
+        self.print_horizontal_maps(mutated_children)
         return next_generation
+    
+    def print_horizontal_maps(self, maps):
+        # Find the maximum number of rows in the maps
+        max_rows = max(len(map_) for map_ in maps)
+
+        # Print the maps horizontally with extra spaces between each map
+        for i in range(max_rows):
+            for map_ in maps:
+                if i < len(map_):
+                    print(''.join(map_[i]), end="     ")
+                else:
+                    print(" " * len(map_[0]), end="     ")
+            print()
+
 
     def select_maps_for_reproduction(self):
         
@@ -178,12 +261,19 @@ class Map:
             # Create the next generation
             self.population = self.evolve_population(selected_maps)
 
+            print("NEW POPULATION: ")
+            self.print_horizontal_maps(self.population)
+
             # Optionally, you can keep track of the best map in each generation
-            best_map = max(self.population, key=lambda x: x.fitness)
-            print(f"Generation {generation + 1}, Best Fitness: {best_map.fitness}")
+            #best_map = max(self.population, key=lambda x: x.fitness)
+            #print(f"Generation {generation + 1}, Best Fitness: {best_map.fitness}")
 
         # Get the optimal map from the final generation
-        self.layout = max(self.population, key=lambda x: x.fitness)
+        self.layout = max(self.population, key=lambda x: self.evaluate_fitness(x))
+        #self.print_map(self.layout)
+        print("FINAL MAP: ")
+        self.print_map(self.layout)
+        #raise Exception("Sorry, no numbers below zero")
         # Optionally, return or save the best map from the final generation
         #return max(population, key=lambda x: x.fitness)
 
@@ -195,17 +285,37 @@ class Map:
         self.genetic_algorithm()
         self.place_players_and_objects()
 
+    def find_empty_coordinates(self, region):
+        rows, cols = len(self.layout)-1, len(self.layout[0])
+        empty_coordinates = []
+        print (rows, cols)
+
+        for row in range(rows):
+            for col in range(cols):
+                if 0 <= row < rows and 0 <= col < cols and self.layout[row][col] == ' ':
+                    # Add the empty coordinate if it's within the valid range
+                    empty_coordinates.append((row, col))
+
+        return empty_coordinates[:4]  # Return the first four empty coordinates
+
     def place_players_and_objects(self):
         self.layout.append(["\n", "SimpleTomato", "\n"])
 
-        lettuce_coordinates = [
-            (5, 1),
-            (4, 1),
-            (4, 4),
-            (2, 4)
-        ]
+        chef_region1 = self.find_empty_coordinates("Region 1")
+        chef_region2 = self.find_empty_coordinates("Region 2")
+        chef2_region1 = self.find_empty_coordinates("Region 1")
+        chef2_region2 = self.find_empty_coordinates("Region 2")
 
-        for x, y in lettuce_coordinates:
+        chef_coordinates = chef_region1 + chef_region2 + chef2_region1 + chef2_region2
+
+        # chef_coordinates = [
+        #     (5, 1),
+        #     (4, 1),
+        #     (4, 4),
+        #     (2, 4)
+        # ]
+
+        for x, y in chef_coordinates:
             self.layout.append([str(x), " ", str(y)])
         # Write the generated layout to the specified file
         with open(self.file_path, 'w') as f:
